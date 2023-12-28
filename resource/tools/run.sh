c53b1049d425e1eb4beaeb4bb681480a7f822baa
Hyunghoon Kim <hyunghki@c1r4s4.42seoul.kr>
2023-12-28 17:15:09 +0900

    more flexible

--- resource/tools/run.sh
+++ resource/tools/run.sh
@@ -3,7 +3,7 @@
 cd $1
 
 for dir in */; do
-	g++ -std=c++17 ${dir}*.cpp 2> ${dir}error
+	g++ -Wall -Werror -Wextra -std=c++17 ${dir}*.cpp 2> ${dir}error
 	if [ $? -eq 0 ]; then
 		for file in ${dir}*.in; do
 			out="${file/%.in/.out}"
@@ -12,6 +12,9 @@ for dir in */; do
 				exit 1
 			fi
 			./a.out < $file > ${out}
+			if [ $? -ne 0 ]; then
+				echo $'\e[91mError: \e[0m'"Failed to run $(basename $file)" >> ${out}
+			fi
 		done
 		rm a.out
 		rm ${dir}error
